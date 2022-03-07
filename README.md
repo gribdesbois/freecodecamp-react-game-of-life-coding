b3d032c7af55fad56f5cab356542b22c95685123
gribdesbois <gribdesbois@gmail.com>
2022-03-07 09:17:25 +0100

    newBoarStatus

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # freecodecamp-react-game-of-life-coding
 https://www.freecodecamp.org/news/coding-the-game-of-life-with-react-7de2385b7356/
+
+#The Game of Life involves a two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, alive or dead. At each step, every cell interacts with its eight adjacent neighbors by following a simple set of rules resulting in births and deaths.
+
+#It’s a zero-player game. Its evolution is determined by its initial state, requiring no further input from players. One interacts with the game by creating an initial configuration and observing how it evolves, or, for advanced players, by creating patterns with particular properties.
+#Rules
+
+    * Any live cell with fewer than two live neighbors dies, as if by underpopulation
+    * Any live cell with two or three live neighbors lives on to the next generation
+    * Any live cell with more than three live neighbors dies, as if by overpopulation
+    * Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
+    
+    
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,45 +1,25 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const totalBoardRows = 40
+const totalBoardColumns = 60
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+const newBoardStatus = (cellStatus = () => Math.random() < 0.3) =>{
+  const grid = []
+  for (let r=0; r < totalBoardRows; r +=1) {
+    grid[r] = []
+    for (let c = 0; c< totalBoardColumns; c+=1)
+  }
+}
+const BoardGrid = () => {}
+const Slider = () => {}
+
+export class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
 
 export default App

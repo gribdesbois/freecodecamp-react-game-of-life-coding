0093e12b0cd9c0b2de1f6d1e12e38b76105e5ebb
gribdesbois <gribdesbois@gmail.com>
2022-03-07 11:24:31 +0100

    handleStep

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -41,11 +41,87 @@ export class App extends Component {
     })
   }
 
+  //! ToggleCellStatus
+  /* Deep clones array to avoid modifying it by ref when updating individual on next line
+  spread syntax goes one level deep while copying the array
+  therefore may be unsuitable for copying multidimensional arrays
+  refs:
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Deep_Clone
+  https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
+  https://stackoverflow.com/questions/728360/how-do-i-correctly-clone-a-javascript-object
+  */
+
+  handleToggleCellStatus = (r, c) => {
+    const toggleBoardStatus = (prevState) => {
+      const clonedBoardStatus = JSON.parse(
+        JSON.stringify(prevState.boardStatus)
+      )
+      clonedBoardStatus[r][c] = !clonedBoardStatus[r][c]
+      return clonedBoardStatus
+    }
+
+    this.setState((prevState) => ({
+      boardStatus: toggleBoardStatus(prevState),
+    }))
+  }
+
   handleNewBoard = () => {
     this.setState({
       boardStatus: newBoardStatus(),
       generation: 0,
     })
+  }
+
+  handleStep = () => {
+    const nextStep = (prevState) => {
+      const { boardStatus } = prevState
+      const clonedBoardStatus = JSON.parse(JSON.stringify(boardStatus))
+
+      const amountTrueNeighbors = (r, c) => {
+        const neighbors = [
+          [-1, -1],
+          [-1, 0],
+          [-1, 1],
+          [0, 1],
+          [1, 1],
+          [1, 0],
+          [1, -1],
+          [0, -1],
+        ]
+        return neighbors.reduce((trueNeighbors, neighbor) => {
+          const x = r + neighbor[0]
+          const y = c + neighbor[1]
+
+          const isNeighborOnBoard =
+            x >= 0 && x < totalBoardRows && y >= 0 && y < totalBoardColumns
+          //! there is no need to count more than 5 alive neighbors
+          if (trueNeighbors < 4 && isNeighborOnBoard && boardStatus[x][y]) {
+            return trueNeighbors + 1
+          } else {
+            return trueNeighbors
+          }
+        }, 0)
+      }
+
+      for (let r = 0; r < totalBoardRows; r += 1) {
+        for (let c = 0; c < totalBoardColumns; c += 1) {
+          const totalTrueNeighbors = amountTrueNeighbors(r, c)
+
+          if (!boardStatus[r][c]) {
+            if (totalTrueNeighbors === 3) clonedBoardStatus[r][c] = true
+          } else {
+            if (totalTrueNeighbors < 2 || totalTrueNeighbors > 3)
+              clonedBoardStatus[r][c] = false
+          }
+        }
+      }
+      return clonedBoardStatus
+    }
+
+    this.setState((prevState) => ({
+      boardStatus: nextStep(prevState),
+      generation: prevState.generation + 1,
+    }))
   }
 
   render() {
